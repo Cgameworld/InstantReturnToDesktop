ac8d1967e0f84e22be058750559b5160f041f295
Cgameworld <49973689+Cgameworld@users.noreply.github.com>
2020-06-10 02:16:24 -0700

    added floating persistant terminate button

--- InstantReturnToDesktop/Main.cs
+++ InstantReturnToDesktop/Main.cs
@@ -4,6 +4,7 @@ using ICities;
 using System;
 using System.Diagnostics;
 using System.Reflection;
+using UnityEngine;
 using Debug = UnityEngine.Debug;
 
 namespace InstantReturnToDesktop
@@ -27,6 +28,11 @@ namespace InstantReturnToDesktop
         {
             harmony = HarmonyInstance.Create(harmonyId);
             harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            //add to load only if xml true
+            GameObject returnToDesktop = new GameObject("ReturnToDesktop");
+            returnToDesktop.AddComponent<PersistentButton>();
+            UnityEngine.Object.DontDestroyOnLoad(returnToDesktop);
         }
 
         public void OnDisabled()
@@ -42,23 +48,29 @@ namespace InstantReturnToDesktop
     {
         static bool Prefix()
         {
-            try
-            {
-                LoadingManager.instance.autoSaveTimer.Stop();
-                Process.GetCurrentProcess().Kill();
-            }
-            catch (Exception e)
-            {
-                Debug.Log("IRD: -\n" + e);
-                Tools.ShowErrorWindow("Instant Return to Desktop Error", e+ "\n\nThis mod was unable to terminate the game executable on your computer\n\nReport this error by uploading your output_log.txt file and paste the link in the steam workshop description of this mod as described here: https://steamcommunity.com/workshop/filedetails/ \n?id=463645931");
-            }
-
-            return false;
+            return Tools.Terminate();
         }
     }
 
     public class Tools
     {
+        public static bool Terminate()
+        {
+            try
+            {
+                LoadingManager.instance.autoSaveTimer.Stop();
+                Process.GetCurrentProcess().Kill();
+                return false;
+            }
+            catch (Exception e)
+            {
+                Debug.Log("IRD: -\n" + e);
+                Tools.ShowErrorWindow("Instant Return to Desktop Error", e + "\n\nThis mod was unable to terminate the game executable on your computer\n\nReport this error by uploading your output_log.txt file and paste the link in the steam workshop description of this mod as described here: https://steamcommunity.com/workshop/filedetails/ \n?id=463645931");
+            }
+
+            return false;
+
+        }
         public static ExceptionPanel ShowErrorWindow(string header, string message)
         {
             ExceptionPanel panel = UIView.library.ShowModal<ExceptionPanel>("ExceptionPanel");
@@ -67,4 +79,35 @@ namespace InstantReturnToDesktop
             return panel;
         }
     }
+
+    public class PersistentButton : MonoBehaviour
+    {
+        //also only call when option enabled!
+        Rect windowBounds;
+
+        void Start()
+        {
+            windowBounds = new Rect(0, 0, 120, 30);
+            // windowBounds = new Rect(0, Screen.height-30, 120, 30);
+        }
+
+        void OnGUI()
+        {
+            Debug.Log("onguicalled!");
+            windowBounds = GUI.Window(321, windowBounds, DoWindow, "");
+        }
+
+        void DoWindow(int windowID)
+        {
+            if (GUI.Button(new Rect(5, 5, 110, 20), "Terminate"))
+            {
+                Tools.Terminate();
+            }
+
+            GUI.DragWindow(new Rect(0, 0, 10000, 20));
+        }
+
+
+    }
+
 }
